cc1d27cd13d828a54d8e8ef5932d3b28f3df43df
WataruMaeda <w.maeda.jp@gmail.com>
2021-09-21 21:36:31 -0700

    chore: cleanup unused pages

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,15 @@
 import React, { Suspense, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { actions } from 'slices/app.slice'
-import { path } from 'utils/const'
 import Fallback from 'components/Fallback'
 import Spinner from 'components/Spinner'
 
-const Auth = React.lazy(() => import('./pages/auth'))
 const Dashboard = React.lazy(() => import('./pages/dashboard'))
 
 function Router() {
   const dispatch = useDispatch()
-  const { checked, loggedIn } = useSelector((state) => state.app)
+  const { checked } = useSelector((state) => state.app)
 
   useEffect(() => {
     dispatch(actions.authenticate())
@@ -28,21 +26,7 @@ function Router() {
   return (
     <BrowserRouter>
       <Suspense fallback={<Fallback />}>
-        {!loggedIn ? (
-          <Switch>
-            <Route path="/">
-              <Auth />
-            </Route>
-            <Redirect to="/" />
-          </Switch>
-        ) : (
-          <Switch>
-            <Route path={path.dashboard}>
-              <Dashboard />
-            </Route>
-            <Redirect to={path.dashboard} />
-          </Switch>
-        )}
+        <Dashboard />
       </Suspense>
     </BrowserRouter>
   )
