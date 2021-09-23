a5a90e41d6124ec81b9535bf823c301a9b172d80
WataruMaeda <w.maeda.jp@gmail.com>
2021-09-23 01:56:10 -0700

    feat: configure google drive picker

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,11 +1,31 @@
+/* eslint-disable no-undef */
 import React from 'react'
+import { useGooglePicker } from 'hooks/useGooglePicker'
 import styles from './dashboard.module.scss'
 
 const Dashboard = () => {
-  console.log('[##] test')
+  const { pickFile } = useGooglePicker({
+    developerKey: process.env.REACT_APP_GOOGLE_DRIVE_DEVELOPER_KEY,
+    clientId: process.env.REACT_APP_GOOGLE_DRIVE_CLIENT_KEY,
+    scope: ['https://www.googleapis.com/auth/drive.file'],
+  })
+
+  const handleOnClick = async () => {
+    try {
+      const file = await pickFile()
+      console.log('[##] file', file)
+    } catch (err) {
+      console.log('[##] err', err)
+    }
+  }
+
   return (
     <div className={styles.root}>
-      <div className={styles.container}>sss</div>
+      <div className={styles.container}>
+        <button type="button" onClick={handleOnClick}>
+          GET FILE
+        </button>
+      </div>
     </div>
   )
 }
